520659ad4415cd11e19402545a26325722f354b6
gorniak <gorniakowski@gmail.com>
2018-12-20 22:31:01 +0100

    Initial picture placement.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,7 +7,9 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <h1>This is home element</h1>
+                <img src="triangle.svg" />
+
+
                 <a class="f3 link dim ba ph3 pv2 mb2 dib black shadow-5" 
                    href="#0"
                    onClick={ this.props.clockReset}

b00b9cb4d35ab724e508f3cf1e4a93c5608ed9fe
gorniak <gorniakowski@gmail.com>
2018-12-23 22:04:59 +0100

    Further work on drag your avatar functionality.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,32 +1,45 @@
 import React from 'react';
-import Draggable from 'react-draggable';
+import { DragDropContainer, DropTarget } from 'react-drag-drop-container';
+
+
 
 
 class Home extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            readyForBro: false
+        }
+    }
+
+    dropped = (e) => {
+        this.setState({readyForBro: true})
+        e.containerElem.style.visibility = 'hidden';
+
+
+
+    }
 
 
     render() {
         return (
             <div>
-                   <Draggable
-        axis="both"
-        handle=".handle"
-        defaultPosition={{x: 100 , y: 200}}
-        position={null}
-        grid={[25, 25]}
-        scale={1}
-        onStart={this.handleStart}
-        onDrag={this.handleDrag}
-        onStop={this.handleStop}>
-        <div>
-          <div className="handle">Drag from here</div>
-          <div>This readme is really dragging on...</div>
-        </div>
-      </Draggable>
-                <div className="h5 yellow ba  fl w-100 pa2 mw5"></div>
+
+                <DragDropContainer targetKey="browar" >
+                    <div>MOrda</div>
+                </DragDropContainer>
+                
+                <DropTarget targetKey="browar" onHit={this.dropped}>
+                    <div className="h5 yellow ba  fl w-100 pa2 mw5">
+                        {this.state.readyForBro && <div>MOrda</div>}
+
+                    </div>
+                </DropTarget>
+                
                 <a class="f3 link dim ba ph3 pv2 mb2 dib black shadow-5 " 
                    href="#0"
-                   onClick={ this.props.clockReset}
+                   onClick={this.props.clockReset}
                    >
                     Browar się odbył !!
                 </a>
